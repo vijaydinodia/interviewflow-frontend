37a05c6a16a5052f92a22af4266505842af0f29e
vijay <vijaydinodia548@gmail.com>
2026-08-22 15:07:05 +0530

    env

--- src/api.js
+++ src/api.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
 
 const api = axios.create({
   baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
   },
+
 });
 
 export { API_URL, api };
